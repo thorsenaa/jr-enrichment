89b04c7b4cb933418ce42e19bd3299cd85cb4643
Annelise Thorsen <annelise.thorsen@gmail.com>
2017-10-01 22:05:02 -0400

    inc

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+//or router = express.Router();
+
+//const nunjucks = require('nunjucks');
 
 const db = require('./db').db;
 const Student = require('./db').Student;
 const Teacher = require('./db').Teacher;
 
+const bodyParser = require('body-parser');
+// app.use(express.static('public'));
+// var env = nunjucks.configure('views', {noCache: true});
+// app.set('view engine', 'html');
+// app.engine('html', nunjucks.render);
+// app.use(bodyParser.urlencoded({extended: false}));
 let PORT = 8080;
 
 app.get("/test", (req, res, next) => {
@@ -15,8 +24,86 @@ app.get("/test", (req, res, next) => {
  Your Route Code Here
 */
 
+//GET all students and return them in json - PASSING
+app.get("/students", (req, res, next) => {
+
+	Student.findAll()
+	.then(students => res.json(students));
+
+})
+
+//GET student by ID # and return a json of that student - NOT WORKING
+app.get("/:studentsId", (req, res, next) => {
+	const id = req.params.student;
+	console.log(id);
+	//console.log("req.params.student", req.params.id)
+		Student.findById({
+			where : id
+		})
+		.then(student => res.json(student));	
+	})
+
+//Adding POST for testing - NOT WORKING
+app.get("/", (req, res, next) => {
+		console.log(req.body)
+		Student.create(req.body)
+		.then(students => res.json(students).sendStatus(201))
+		.catch(next);
+	})
+
+//UPDATE a student's teacher and return a status code of 204 - INC
+app.put("/:studentId", (req, res, next) => {
+	const id = req.params.student
+	Student.findById(id)
+	//?
+})
+
+//DELETE a student and return a status code of 202
+app.delete('/:studentId', (req, res, next) =>  {
+	const id = req.params.student;
+	Student.destroy({
+		where: {
+			id
+		}
+	})
+	Student.findById({
+		where: {
+			id
+		}
+	})
+	.then(() => {
+		res.sendStatus(202)
+	})
+})
+
 db.sync({force: true})
 .then(() => {
 	console.log('db synced')
 	app.listen(PORT, () => console.log(`server listening on port ${PORT}`))
+
+	const students = Promise.all([
+		{name: 'ashi', gpa: 2.7},
+		{name: 'emily', gpa: 4.0},
+		{name: 'bob', gpa: 3.7},
+		{name: 'donnie', gpa: 0},
+	].map(data => Student.create(data)))
+
+	const teachers = Promise.all([
+		{name: 'dan'},
+		{name: 'david'}
+	].map(data => Teacher.create(data)))
+
+	Promise.all([students, teachers])
+		.then(([
+			[ashi, emily, bob, donnie],
+			[dan, david],
+		]) => Promise.all([
+			ashi.setTeacher(dan),
+			bob.setTeacher(david),
+			emily.setTeacher(david),
+			dan.addStudent(donnie),
+		]))
+		.then(() => {
+			console.log('database seeded.')
+		})
 });
--- db.js
+++ db.js
@@ -1,21 +1,45 @@
 const Sequelize = require('sequelize');
 
-
 const db = new Sequelize('postgres://localhost/juniorenrichment', {
   logging: false
 });
 
+/* STUDENT MODEL CODE HERE */
+const Student = db.define('student', {
+	name: {
+		type: Sequelize.STRING,
+		unique: false
+	},
+	gpa: {
+		type: Sequelize.DECIMAL(2,1)
+	}
+},
+	{
+		getterMethods: {
+			grade: function (){
+				return ['F', 'D', 'C', 'B', 'A']
+				[Math.floor(this.gpa)]
+			}
+		}
+	}
+)
 
-const Student = db.define("student" , {
-	/* STUDENT MODEL CODE HERE */
+/*
+Class Method
+*/
+Student.perfect = function() {
+	return Student.findAll({
+		where: { gpa:4.0 },
+	})
+}
 
-})
-	
-
+/* TEACHER MODEL CODE HERE */
 const Teacher = db.define('teacher', {
-	/* TEACHER MODEL CODE HERE */
-
+	name: Sequelize.STRING,
+	subject: Sequelize.STRING
 });
 
+Student.belongsTo(Teacher)
+Teacher.hasMany(Student)
 
 module.exports = {db, Student, Teacher}
